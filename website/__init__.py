60fb327b410f2f784cd6a28a367b4c9a58326d1c
Deepak Basel <deepak.bashel77@gmail.com>
2024-06-30 19:01:44 +0545

    Database created!!

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db= SQLAlchemy()
+DB_NAME= 'database.sqlite3'
+
+
+def create_database():
+    db.create_all()
+    print('Database Created')
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'willofD'
+    app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{DB_NAME}'
+    
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
@@ -14,7 +26,9 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(admin, url_prefix='/')
     
-    
+    with app.app_context():
+        create_database()
+        
     
     return app
     
